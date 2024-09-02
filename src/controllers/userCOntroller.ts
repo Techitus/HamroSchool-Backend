2d947ac85fb07d64c8aecb0a40d12ec2cad62465
Kamal Paudel <techitus07@gmail.com>
2024-09-02 13:52:05 +0545

    user controller

--- src/controllers/userCOntroller.ts
+++ src/controllers/userCOntroller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
-
+import User from "../database/models/user";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 class AuthController {
     public static async loginUser(req:Request,res:Response):Promise<void>{
         const { email,password } = req.body
@@ -9,7 +13,36 @@ class AuthController {
             })
             return
         }
+        const [user] = await User.findAll({
+            where :{
+                email : email
+            }
+
+        })
+        if(!user){
+            res.status(400).json({
+                message : "User not found with the given email"
+            })
+            return
+        }
+        const isMatched = await bcrypt.compareSync(password,user.password)
+        if(!isMatched){
+            res.status(400).json({
+                message : "Invalid password"
+            })
+            return
+        }
+        if(isMatched){
+            const token = jwt.sign({id:user.id}, process.env.JWT_SECRET as string,{
+                expiresIn: process.env.JWT_EXPIRES_IN as string
+            })
+            res.status(200).json({
+                message : "Login successful",
+                user : token
+            })
+        }
         
+    
         
     }
 }
