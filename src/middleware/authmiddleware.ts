0975a09e39edeec14518a93b2d119b5d68248953
Kamal Paudel <techitus07@gmail.com>
2024-09-03 12:51:41 +0545

    authmiddleware complete

--- src/middleware/authmiddleware.ts
+++ src/middleware/authmiddleware.ts
@@ -1,45 +1,69 @@
-import { NextFunction, Request,Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import User from "../database/models/user";
-dotenv.config()
-interface AuthRequest extends Request{
+
+dotenv.config();
+
+interface AuthRequest extends Request {
     user?: {
-        id :string,
-        email :string,
-        password :string,
-        role :string
+        id: string;
+        email: string;
+        password: string;
+        role: string;
     }
 }
 
+enum Role {
+    Admin = "admin",
+}
 
 class AuthMiddleware {
-    isAuthenticated(req:AuthRequest, res:Response, next:NextFunction){
-        const token = req.headers.authorization
-        if(!token || token === undefined){
+    isAuthenticated(req: AuthRequest, res: Response, next: NextFunction) {
+        const token = req.headers.authorization;
+        if (!token || token === undefined) {
             res.status(401).json({
-                message: "Unauthorized token"
-            })
-            return
+                message: "Unauthorized token",
+            });
+            return;
         }
-        console.log(process.env.JWT_SECRET)
-        jwt.verify(token, process.env.JWT_SECRET as string, async(err, decoded:any):Promise<void> => {
-            if(err){
+        
+        jwt.verify(token, process.env.JWT_SECRET as string, async (err, decoded: any): Promise<void> => {
+            if (err) {
                 res.status(401).json({
-                    message: "Invalid token"
-                })
-                return
+                    message: "Invalid token",
+                });
+                return;
             }
-          const userData = await User.findByPk(decoded.id)
-          if(!userData){
-            res.status(400).json({
-                message : "No User with that token"
-            })
-            return
-          }
-          req.user = userData
-          next()
+            const userData = await User.findByPk(decoded.id);
+            try {
+                if (!userData) {
+                    res.status(400).json({
+                        message: "No User with that token",
+                    });
+                    return;
+                }
+                req.user = userData;
+                next();
+            } catch (err) {
+                res.status(500).json({
+                    message: "Something went wrong",
+                });
+            }
+        });
+    }
 
-        })
+    restrictTo(role: Role) {
+        return (req: AuthRequest, res: Response, next: NextFunction) => {
+            if (req.user && req.user.role === role) {
+                next();
+            } else {
+                res.status(401).json({
+                    message: "You are unauthorized to access this route",
+                });
+            }
+        };
     }
 }
+
+export default AuthMiddleware;
