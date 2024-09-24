2c99c5c1b0cdf1d8a9a47c679aa70876695dcae8
Kamal Paudel <techitus07@gmail.com>
2024-09-24 12:56:25 +0545

    home service and testimonial controller

---
+++ src/controllers/homeserviceController.ts
@@ -0,0 +1,59 @@
+import { Response } from "express";
+import { AuthRequest } from "../middleware/authMiddleware";
+import HomeService from "../database/models/homeservice";
+
+class HomeServiceController{
+    public static async addHomeService(req:AuthRequest,res:Response):Promise<void>{
+        const{title,description,status} = req.body;
+        const HomeServices = await HomeService.create({
+            title,
+            description,
+            status
+        })
+        res.status(201).json({
+            message:"Home Testimonial Added Successfully",
+            data:HomeServices
+        })
+    }
+    public static async getHomeService(req:AuthRequest,res:Response):Promise<void>{
+        const HomeServices = await HomeService.findAll({
+            where:{
+                status:'show'
+            }
+        })
+        res.status(200).json({
+            message:"Home Testimonial Fetched Successfully",
+            data:HomeServices
+        })
+    }
+    public static async updateHomeService(req:AuthRequest,res:Response):Promise<void>{
+        const {id} = req.params;
+        const {title,description,status} = req.body;
+        const updated = await HomeService.update({
+            title,
+            description,
+            status
+        },{
+            where:{
+                id
+            }
+        })    
+        res.status(200).json({  
+            message:"Welcome Text Updated Successfully",
+            data:updated
+        })
+ 
+}
+public static async deleteHomeService(req:AuthRequest,res:Response):Promise<void>{
+    const {id} = req.params;
+ await HomeService.destroy({
+        where:{
+            id
+        }
+    })
+    res.status(200).json({
+        message:"Home Testimonial Deleted Successfully",
+    })
+}
+}
+export default HomeServiceController
---
+++ src/controllers/hometestiController.ts
@@ -0,0 +1,59 @@
+import { Response } from "express";
+import { AuthRequest } from "../middleware/authMiddleware";
+import HomeTestimonial from "../database/models/hometestimon";
+
+class HomeTestimonialController{
+    public static async addHomeTestimonial(req:AuthRequest,res:Response):Promise<void>{
+        const{title,description,status} = req.body;
+        const homeTestimonial = await HomeTestimonial.create({
+            title,
+            description,
+            status
+        })
+        res.status(201).json({
+            message:"Home Testimonial Added Successfully",
+            data:homeTestimonial
+        })
+    }
+    public static async getHomeTestimonial(req:AuthRequest,res:Response):Promise<void>{
+        const homeTestimonial = await HomeTestimonial.findAll({
+            where:{
+                status:'show'
+            }
+        })
+        res.status(200).json({
+            message:"Home Testimonial Fetched Successfully",
+            data:homeTestimonial
+        })
+    }
+    public static async updateHomeTestimonial(req:AuthRequest,res:Response):Promise<void>{
+        const {id} = req.params;
+        const {title,description,status} = req.body;
+        const updated = await HomeTestimonial.update({
+            title,
+            description,
+            status
+        },{
+            where:{
+                id
+            }
+        })    
+        res.status(200).json({  
+            message:"Welcome Text Updated Successfully",
+            data:updated
+        })
+ 
+}
+public static async deleteHomeTestimonial(req:AuthRequest,res:Response):Promise<void>{
+    const {id} = req.params;
+    const homeTestimonial = await HomeTestimonial.destroy({
+        where:{
+            id
+        }
+    })
+    res.status(200).json({
+        message:"Home Testimonial Deleted Successfully",
+    })
+}
+}
+export default HomeTestimonialController
